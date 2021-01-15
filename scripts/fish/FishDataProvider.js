5f9c803134f6e610619f082f265f31e095baa7c5
kylieanyce <kylieanyce@gmail.com>
2021-01-15 15:03:04 -0600

    added automation to fish list

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -4,7 +4,7 @@ const fishCollection = [
         name: "Nemo",
         species: "Amphiprioninae",
         location: "Great Barrier Reef, Australia",
-        size: "couple inches",
+        size: 9,
         food: "Seafood",
     },
     {
@@ -12,7 +12,7 @@ const fishCollection = [
         name: "Nemo",
         species: "Amphiprioninae",
         location: "Great Barrier Reef, Australia",
-        size: "couple inches",
+        size: 5,
         food: "Seafood",
     },
     {
@@ -20,7 +20,7 @@ const fishCollection = [
         name: "Nemo",
         species: "Amphiprioninae",
         location: "Great Barrier Reef, Australia",
-        size: "couple inches",
+        size: 4,
         food: "Seafood",
     },
     {
@@ -28,7 +28,7 @@ const fishCollection = [
         name: "Nemo",
         species: "Amphiprioninae",
         location: "Great Barrier Reef, Australia",
-        size: "couple inches",
+        size: 5,
         food: "Seafood",
     },
     {
@@ -36,10 +36,49 @@ const fishCollection = [
         name: "Nemo",
         species: "Amphiprioninae",
         location: "Great Barrier Reef, Australia",
-        size: "couple inches",
+        size: 3,
         food: "Seafood",
     },
 ];
 export const useFish = () => {
     return fishCollection.slice()
 };
+
+export const mostHolyFish = () => {
+    // 3, 6, 9, 12, etc... fish
+    const fishThreeInches = []
+
+    for (const fish of fishCollection) {
+        if (fish.size % 3 === 0){
+            fishThreeInches.push(fish)
+        }
+    }
+    console.log("mostHolyFish", fishThreeInches)
+    return fishThreeInches;
+}
+
+
+export const semiHolyFish = () => {
+    // 5, 10, 15, 20, 25, etc... fish
+    const fishFiveInches = []
+
+    for (const fish of fishCollection) {
+        if (fish.size % 5 === 0) {
+            fishFiveInches.push(fish);
+        }
+    }
+    console.log("semi holy fish", fishFiveInches)
+    return fishFiveInches;
+}
+
+export const nonHolyFish = () => {
+    // Any fish not a multiple of 3 or 5
+    const notHolyFish = []
+
+    for (const fish of fishCollection) {
+        if (fish.size % 3 !== 0 && fish.size % 5 !== 0) {
+            notHolyFish.push(fish);
+        }
+    }
+    return notHolyFish
+}
